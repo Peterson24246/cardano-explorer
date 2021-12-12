d31ceb870dd5b26d17989e65588b6841c8e519f3
Dylan Peterson <peterson24246@yahoo.com>
2021-12-12 15:23:32 -0600

    Testing api call

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [block, setBlock] = useState('')
+  useEffect(()=>{
+    fetch('https://cardano-explorer-backend.herokuapp.com/api/')
+      .then(result => {
+        setBlock(result)
+        console.log(block)
+      })
+  },[block])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Latest Block</h1>
+      <p></p>
     </div>
   );
 }
